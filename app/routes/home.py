eb8ef9d6e7136849b3dd16fbeed9cfede54ac5a4
Yurii Shevchenko <skygtr5612@gmail.com>
2022-07-13 09:02:00 +0200

    feat/add_pydantic

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.services.test_service import get_test_something
+from app.models.schemas import User
+from app.models.schemas import SignUp
 
 router = APIRouter(tags=['home'])
 
@@ -12,4 +14,9 @@ async def home_page():
 @router.get('/test')
 async def test_page(id: int):
     return await get_test_something(id)
-    
+
+
+
+@router.get('/SignUp')
+    def SignUp(email: str, password: str)
+    return {email: str, password: str}
